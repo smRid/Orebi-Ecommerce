b133eeb6843014b7b0b47ff6887c9339fe620f89
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-04-04 16:49:32 +0600

    Code update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,21 +31,21 @@ const Navbar = () => {
     }
 
   return (
-    <nav className='bg-[#ffffff] py-3'>
-        <Container className='flex items-center py-3 justify-between px-2 lg:px-0 relative'>
+    <nav className='bg-[#ffffff] py-5'>
+        <Container className='flex items-center py-3 justify-between relative px-3 lg:px-0 '>
             <div className="logo">
-                <Image href='/' src='src/assets/logo.png'/>
+                <Image href='/' src='src/assets/Logo.png' />
             </div>
 
             {
                 show && (
-                    <div className="menu py-7 px-4 lg:py-0 absolute lg:px-0 left-0 top-full bg-slate-200 lg:static lg:bg-transparent flex w-full justify-center ">
-                      <ul className='flex z-10 items-center flex-col gap-y-2 lg:flex-row gap-x-[40px]'>
-                         <Listitem href="/" text="Home"/>
-                         <Listitem href="/" text="Shop"/>
-                         <Listitem href="/" text="About"/>
-                         <Listitem href="/" text="Contacts"/>
-                        <Listitem href="/" text="Journal"/>
+                    <div className="menu_item py-7 px-4 lg:py-0 absolute lg:px-0 left-0 top-full bg-[#b4d3d4] lg:static lg:bg-transparent flex w-full justify-center z-50">
+                      <ul className='flex items-center flex-col gap-y-2 lg:flex-row gap-x-[40px]'>
+                         <Listitem className='hover:text-[#000]' href="/" text="Home"/>
+                         <Listitem className='hover:text-[#000]' href="/" text="Shop"/>
+                         <Listitem className='hover:text-[#000]' href="/" text="About"/>
+                         <Listitem className='hover:text-[#000]' href="/" text="Contacts"/>
+                        <Listitem className='hover:text-[#000]' href="/" text="Journal"/>
                 </ul>
             </div>)
             }
@@ -60,6 +60,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-
- 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,44 +1,115 @@
-import React from 'react'
-import Container from './layer/Container';
+import React, { useEffect, useState } from 'react'
+import Container from './layer/Container'
 import { HiOutlineBars3BottomLeft } from "react-icons/hi2";
-import { FaSearch, FaUser,FaCaretDown,FaShoppingCart } from "react-icons/fa";
-
-
-
+import { FaSearch,FaUser,FaAngleRight, FaShoppingCart } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import Listitem from './layer/Listitem';
 
 
 
 const SearchBar = () => {
+
+  let [show, setShow] = useState (false)
+
+  useEffect(()=>{
+    if(window){
+      setShow(false)
+  }else{
+      setShow(true)
+  }
+  },[])
+
+
+  let catagory = ()=>{
+    setShow(!show)
+  }
+
+
+
+  let [showx, setShowy] = useState (false)
+
+  useEffect(()=>{
+    if(window){
+      setShowy(false)
+  }else{
+      setShowy(true)
+  }
+  },[])
+
+  let acc = ()=>{
+    setShowy(!showx)
+  }
+
   return (
-    <div className='py-6 bg-[#F5F5F3]'>
-       <Container className="flex justify-between items-center">
+    <div className='lg:py-10 py-2 bg-[#F5F5F3]'>
 
-        <div className="">
+      <Container className='flex justify-between items-center font-DM flex-row px-3 lg:px-0'>
 
-         <p className='flex items-center gap-x-2 font-DM text-sm text-[#262626]'><HiOutlineBars3BottomLeft /> Shop by category</p>
+        <div onClick={catagory} className='relative order-1'>
+          <p className='flex items-center gap-x-2  text-[#262626] text-sm cursor-pointer'>
+            <HiOutlineBars3BottomLeft />
+            <span className="hidden lg:inline">Shop by category</span>
+            <span className="inline lg:hidden mr-4">Category</span>
+          </p>
+{
+            show && (
+
+              <ul className='absolute top-full left-0 bg-[#262626] text-[#767676] translate-y-6 lg:py-4 py-2 lg:w-[263px] w-[200px] lg:px-5 px-2 flex flex-col lg:gap-[16px] gap-[8px] z-50'>
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Accesories ' /> 
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Furniture' />
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Electronics' />
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Clothes' />
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Bags' />
+
+              <Listitem className='text-[#767676] hover:text-[#FFFFFF] py-4 font-[400] hover:px-8 mt-4' href='/' text='Home appliances' />
+
+            </ul>
+
+
+
+            )
+          }
+
+
         </div>
 
-        <div className="relative">
-          <input className='w-[621px] py-4 px-5 border-0 outline-0 placeholder:font-DM placeholder:text-sm placeholder:text-[#C4C4C4]' placeholder='Search Products' type="search" />
-          <FaSearch className='absolute right-5 top-1/2 translate-y-[-50%]' />
+        <div className='relative lg:order-2 order-3'>
+          <input type="text" placeholder='Search Products' className='lg:w-[601px] w-full lg:py-4 py-1 px-5 bg-white outline-none' />
+          <FaSearch className='absolute right-1 lg:right-5 top-1/2 translate-y-[-50%]' />
 
         </div>
 
-        <div className="flex items-center gap-x-10">
-          <div className='flex items-center gap-x-1'>
-            <FaUser/>
-            <FaCaretDown />
+
+        <div className='flex items-center gap-x-2 lg:gap-x-10 cursor-pointer lg:order-3 order-2'>
+          <div className='flex items-center gap-x-1 '>
+            <FaUser />
+            <IoMdArrowDropdown />
+{/* 
+            <div onClick={acc} className='relative' >
+              <ul className=' right-0 w-[200px] absolute flex flex-col gap-[25px] text-center translate-y-8' >
+                <Listitem className="pt-[17px] pb-[15px] pl-[59px] pr-[58px] bg-black hover:bg-white text-[#fff] hover:text-black" href="/" text="My Account"/>
+
+                <Listitem className="py-4 pt-[17px] pb-[15px] pl-[72px] pr-[75px]  text-[#272727] hover:text-white bg-[#ffffff] hover:bg-black font-[400]" href="/" text="Log Out" />
+              </ul>
+            </div> */}
+
+
+
+
+
           </div>
-
-          <div>
+          <div className='pr-3'>
             <FaShoppingCart />
-          </div>
 
+          </div>
         </div>
 
-
-
-       </Container>
+      </Container>
 
     </div>
   )
